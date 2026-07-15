63b3cf2bbec723286c041c5546c2e0843c31df44
zihaodream <zihaozhao363@gmail.com>
2026-07-15 21:07:48 +0800

    Delete app directory

--- app/UTK-WiFiOnly-GUI.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0UTK-WiFiOnly-GUI.ps1"
--- app/stop.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -ExecutionPolicy Bypass -File "%~dp0UTK-WiFiOnly-App.ps1" -Action Stop
-pause
